4ec7c4fb676c38ad58c56161d26e556dea2e1b12
NathanC <cnlnat@yahoo.com>
2019-10-26 02:52:01 -0400

    The first commit to start.

---
+++ ConcertsTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConcertsTracker.Startup))]
+namespace ConcertsTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
